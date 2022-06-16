a5223cba6bb0624b710c5f00fa631ff5086d2b00
Egor Alimpiev <alimpiev@stanford.edu>
2022-06-15 20:52:57 -0700

    separated simulation logic from sampling details

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,56 @@
 use rand::seq::SliceRandom;
-use rand::thread_rng;
+use rand::prelude::*;
 
 fn main() {
+    run(wf, 500, 300, 0.5);
+}
 
-    let num_generations: usize  = 100;
-    let n: usize = 300;
-    let p_init: f64 = 0.5;
-    
-    //initialize the two main arrays for the simulation
-    let mut parents = vec![0.0; n];
-    let mut offspring = vec![0.0; n];
+fn run(iteration: fn(&mut Vec<f64>), num_generations: usize, n: usize, p_init: f64) {
+    let mut population = vec![0.0; n];
 
     let init_count = (p_init*n as f64) as usize;
 
     // fill the parental generation with values
     for i in 0..init_count {
-        parents[i] = 1.0;
+        population[i] = 1.0;
     }
 
     for _ in 0..num_generations {
-        generation(&mut parents, &mut offspring);
+        iteration(&mut population);
 
         let mut s = 0.0;
 
-        for i in 0..parents.len() {
-            s += parents[i];
+        for i in 0..population.len() {
+            s += population[i];
         }
 
         println!("{}", s/(n as f64));
     }
 }
 
-fn generation(parents: &mut Vec<f64>, offspring: &mut Vec<f64>) {
-    let mut rng = thread_rng();
+fn wf_classical(parents: &mut Vec<f64>, offspring: &mut Vec<f64>) {
+    let mut rng = rand::thread_rng();
     for i in 0..parents.len() {
         offspring[i] = *parents.choose(&mut rng).unwrap();
     }
     parents.clone_from(&offspring);
+}
+
+fn binomial_sample(buffer: &mut Vec<f64>, p: f64) {
+    let mut rng = rand::thread_rng();
+    for i in 0..buffer.len() {
+        let x: f64 = rng.gen();
+        if x > p {
+            buffer[i] = 0.0;
+        } else {
+            buffer[i] = 1.0;
+        }
+    }
+}
+
+fn wf(population: &mut Vec<f64>) {
+    let p: f64 = population.iter().sum::<f64>()/population.len() as f64;
+    println!("p is {}", p);
+    binomial_sample(population, p);
 }
 
